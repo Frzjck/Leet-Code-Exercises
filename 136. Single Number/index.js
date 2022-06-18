09e3451a87e0bc3d3959b54bf68f26dac7ed13a0
Frzjck <max.tuzenko@gmail.com>
2022-06-18 19:14:17 +0200

    refactor number 136, add Bitwise XOR solution

--- 136. Single Number/index.js
+++ 136. Single Number/index.js
@@ -1,7 +1,7 @@
 // Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
 // You must implement a solution with a linear runtime complexity and use only constant extra space.
-
+const arr = [4, 1, 2, 1, 2];
 const singleNumber = (nums) => {
 	const numSet = new Set();
 	for (let i = 0; i < nums.length; i++) {
@@ -10,3 +10,11 @@ const singleNumber = (nums) => {
 	}
 	return [...numSet][0];
 };
+
+const singleNumber2 = (nums) => {
+	let singleNum = 0;
+	for (let i = 0; i < nums.length; i++) singleNum = singleNum ^ nums[i];
+	return singleNum;
+};
+
+singleNumber2(arr);
