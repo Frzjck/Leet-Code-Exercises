8665d639c828c7de42bd5894d7ffd1843b5e3740
Frzjck <max.tuzenko@gmail.com>
2022-06-19 17:34:53 +0200

    feat number 144 iterative

--- 145. Binary Tree Postorder Traversal/index.mjs
+++ 145. Binary Tree Postorder Traversal/index.mjs
@@ -1,7 +1,20 @@
 import * as tree from "../leet_workpieces/BinaryTree.mjs";
 import * as list from "../leet_workpieces/LinkedLists.mjs";
 
+// const postorder = (root) => {
+// 	if (!root) return [];
+// 	return [...postorder(root.left), ...postorder(root.right), root.val];
+// };
+
 const postorder = (root) => {
 	if (!root) return [];
-	return [...postorder(root.left), ...postorder(root.right), root.val];
+	const stack = [root];
+	const result = [];
+	while (stack.length) {
+		const current = stack.pop();
+		result.unshift(current.val);
+		if (current.left) stack.push(current.left);
+		if (current.right) stack.push(current.right);
+	}
+	return result;
 };
