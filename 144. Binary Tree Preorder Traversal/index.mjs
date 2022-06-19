4e477132c2fd2f67e9742c94c2e44bf5095c1741
Frzjck <max.tuzenko@gmail.com>
2022-06-19 17:13:53 +0200

    add number 144 recursive

--- 144. Binary Tree Preorder Traversal/index.mjs
+++ 144. Binary Tree Preorder Traversal/index.mjs
@@ -2,14 +2,7 @@ import * as tree from "../leet_workpieces/BinaryTree.mjs";
 
 // Given the root of a binary tree, return the preorder traversal of its nodes' values.
 
-class TreeNode {
-	constructor(val) {
-		this.val = val;
-		this.left = null;
-		this.right = null;
-	}
-}
-
-const arr = [1, 2, 3, 4, 5, 1, 2, 4, 6];
-
-console.log(tree.treeReader(tree.arrayToTree(arr)));
+const postorder = (root) => {
+	if (!root) return [];
+	return [root.val, ...postorder(root.left), ...postorder(root.right)];
+};
