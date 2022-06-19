559e1434c044aefe24171715fd0ed30302f8c932
Frzjck <max.tuzenko@gmail.com>
2022-06-19 17:17:57 +0200

    feat: number 144 iterative

--- 144. Binary Tree Preorder Traversal/index.mjs
+++ 144. Binary Tree Preorder Traversal/index.mjs
@@ -2,7 +2,20 @@ import * as tree from "../leet_workpieces/BinaryTree.mjs";
 
 // Given the root of a binary tree, return the preorder traversal of its nodes' values.
 
+// const postorder = (root) => {
+// 	if (!root) return [];
+// 	return [root.val, ...postorder(root.left), ...postorder(root.right)];
+// };
+
 const postorder = (root) => {
 	if (!root) return [];
-	return [root.val, ...postorder(root.left), ...postorder(root.right)];
+	const stack = [root];
+	const result = [];
+	while (stack.length) {
+		const current = stack.pop();
+		result.push(current.val);
+		if (current.right) stack.push(current.right);
+		if (current.left) stack.push(current.left);
+	}
+	return result;
 };
